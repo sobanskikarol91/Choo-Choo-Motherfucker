099ee285fd7fdb8cb9b2d27f54e02f6c34b07154
DESKTOP-FALGG2T\Karol Sobanski <sobanskikarol91@gmail.com>
2018-03-05 17:49:58 +0100

    others

---
+++ CrazyTrain/Assets/Pavel/Scripts/GameManagament.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagament : MonoBehaviour
+{
+    public SpriteRenderer imageHolder;
+    public Sprite firstImage;
+    public Sprite secondImage;
+    public Sprite thirdImage;
+    public Sprite forthImage;
+    public Sprite fifthImage;
+    public Sprite sixthImage;
+
+    int couter = 1;
+
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (couter == 1) imageHolder.sprite = secondImage;
+            if (couter == 2) imageHolder.sprite = thirdImage;
+            if (couter == 3) imageHolder.sprite = forthImage;
+            if (couter == 4) imageHolder.sprite = fifthImage;
+            if (couter == 5) imageHolder.sprite = sixthImage;
+            if (couter == 6) SceneManager.LoadScene("Karol");
+            couter++;
+        }
+    }
+
+}
---
+++ CrazyTrain/Assets/Pavel/Scripts/UIScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+
+
+    public void StartGame()
+    {
+            SceneManager.LoadScene("Karol");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+    
+    public void StartScene()
+    {
+        SceneManager.LoadScene("Start");
+    }
+    public void StartTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+	
+}
